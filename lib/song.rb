7cf5867978b92047d8f854e7ac254ea879d7ca45
John Guest <jcguest90@gmail.com>
2019-09-30 05:12:53 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -1,7 +1,7 @@
 require 'pry'
 class Song 
   
-  extend Concerns::Findable
+  #extend Concerns::Findable
 
   attr_accessor :name
   attr_reader :artist, :genre 
@@ -50,4 +50,22 @@ class Song
     find_by_name(name) || create(name)
   end
   
+  def self.new_from_filename(name)
+    splits = name.split(" - ")
+    artist_name = splits[0].strip
+    title = splits[1].strip
+    genre_name = splits[2].gsub(".mp3", "").strip
+
+    artist = Artist.find_or_create_by_name(artist_name)
+    genre = Genre.find_or_create_by_name(genre_name)
+
+    self.new(title, artist, genre)
+
+    #binding.pry
+  end
+
+  def self.create_from_filename(filename)
+    new_from_filename(filename).tap{ |s| s.save }
+  end
+
 end
